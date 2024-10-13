753cedf3dc02b8f6ae92ec9d12dd8f1080477030
myt-ml <maltahanml@gmail.com>
2024-10-13 09:41:28 +0300

    match exp

--- index.php
+++ index.php
@@ -17,78 +17,15 @@
   </head>
   <body>
     <h1> recommended books</h1>
-   /* <?php 
-      $bookStore = [
-        "fiction" => [
-          "The Lord of the Rings" => [
-            "author" => "J.R.R. Tolkien",
-            "price" => 29.99,
-            "inStock" => true,
-          ],
-          "Harry Potter and the Philosopher's Stone" => [
-            "author" => "J.K. Rowling",
-            "price" => 9.99,
-            "inStock" => false, 
-          ],
-        ],
-        "nonfiction" => [
-          "Sapiens: A Brief History of Humankind" => [
-            "author" => "Yuval Noah Harari",
-            "price" => 15.99,
-            "inStock" => true,
-          ],
-        ],
-      ];
+    <?php
+    $paymentstatus = 1;
+    switch($paymentstatus){
+      case 0:
+        echo '<p>unpayed</p>';
+      case 1:'<p>payed</p>';  
+    } 
+  
 
-      $students = [
-        "Alice" => [
-          "id" => "A123",
-          "grades" => [
-            "math" => 90,
-            "science" => 85,
-            "history" => 92,
-          ],
-        ],
-        "Bob" => [
-          "id" => "B456",
-          "grades" => [
-            "math" => 78,
-            "science" => 88,
-            "history" => 75,
-          ],
-        ],
-      ];
-      
-      // Example 3: Product catalog with variations
-      
-      $products = [
-        "T-shirt" => [
-          "price" => 19.99,
-          "variations" => [
-            "color" => ["red", "blue", "green"],
-            "size" => ["S", "M", "L", "XL"],
-          ],
-        ],
-        "Hoodie" => [
-          "price" => 34.99,
-          "variations" => [
-            "color" => ["black", "gray", "navy"],
-            "size" => ["M", "L", "XL"],
-          ],
-        ],
-      ];
-      
-      ?>
-      
-      
-        <?php foreach($bookStore as $book1) : ?>
-         
-          <?= $book1['fiction']; ?>
-          
-        <?php endforeach; ?>
-         -->//*/
-  
-  
-   
+?>  
   </body>
 </html>
